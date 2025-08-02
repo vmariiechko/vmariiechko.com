5f66f4682127e9c460b470f08ea3dca621d7fa3c
Vadym Mariiechko <vadym.mariiechko@addepto.com>
2025-08-02 20:39:04 +0200

    Create sitemap and robots.txt files

--- .eleventy.js
+++ .eleventy.js
@@ -1,40 +1,57 @@
-const shortcodes = require('./_11ty/shortcodes.js');
 const { DateTime } = require('luxon');
 
+const shortcodes = require('./_11ty/shortcodes.js');
+const filters = require('./_11ty/filters.js');
+
 module.exports = function (eleventyConfig) {
-  // Register Shortcodes
-  Object.keys(shortcodes).forEach((shortcodeName) => {
-    eleventyConfig.addShortcode(shortcodeName, shortcodes[shortcodeName]);
-  });
+    // Don't process drafts in production
+    if (process.env.NODE_ENV === 'production') {
+        eleventyConfig.ignores.add('src/content/drafts/**/*.md');
+    }
 
-  // Passthrough copy for static assets
-  eleventyConfig.addPassthroughCopy({ 'src/static/favicons': 'favicons' });
-  eleventyConfig.addPassthroughCopy('src/robots.txt');
-  eleventyConfig.addPassthroughCopy('src/manifest.webmanifest');
-  eleventyConfig.addPassthroughCopy('src/content/posts/**/*.jpg');
-  eleventyConfig.addPassthroughCopy('src/content/posts/**/*.png');
+    // Register Shortcodes
+    Object.keys(shortcodes).forEach((shortcodeName) => {
+        eleventyConfig.addShortcode(shortcodeName, shortcodes[shortcodeName]);
+    });
 
-  // Add a filter for readable dates
-  eleventyConfig.addFilter('readableDate', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
-      'dd LLL yyyy'
-    );
-  });
+    // Register Filters
+    Object.keys(filters).forEach((filterName) => {
+        eleventyConfig.addFilter(filterName, filters[filterName]);
+    });
 
-  // Create a collection of posts
-  eleventyConfig.addCollection('posts', (collectionApi) => {
-    return collectionApi.getFilteredByGlob('src/content/posts/**/*.md');
-  });
+    // Passthrough copy for static assets
+    eleventyConfig.addPassthroughCopy({ 'src/static/favicons': 'favicons' });
+    eleventyConfig.addPassthroughCopy('src/robots.txt');
+    eleventyConfig.addPassthroughCopy('src/manifest.webmanifest');
+    eleventyConfig.addPassthroughCopy('src/content/posts/**/*.jpg');
+    eleventyConfig.addPassthroughCopy('src/content/posts/**/*.png');
 
-  return {
-    dir: {
-      input: "src",
-      output: "_site",
-      includes: "_includes",
-      data: "_data",
-    },
-    templateFormats: ["md", "njk", "html"],
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
-  };
+    // Add a filter for readable dates
+    eleventyConfig.addFilter('readableDate', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
+            'dd LLL yyyy'
+        );
+    });
+
+    // Create a collection of posts
+    eleventyConfig.addCollection('posts', (collectionApi) => {
+        return collectionApi.getFilteredByGlob('src/content/posts/**/*.md').filter(post => !post.data.draft);
+    });
+
+    // Create a collection of drafts
+    eleventyConfig.addCollection('drafts', (collectionApi) => {
+        return collectionApi.getFilteredByGlob('src/content/drafts/**/*.md');
+    });
+
+    return {
+        dir: {
+            input: "src",
+            output: "_site",
+            includes: "_includes",
+            data: "_data",
+        },
+        templateFormats: ["md", "njk", "html"],
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+    };
 };
---
+++ _11ty/filters.js
@@ -0,0 +1,28 @@
+const { DateTime } = require('luxon');
+
+const now = new Date();
+
+const helpers = {
+  isLivePost: function (p) {
+    return process.env.NODE_ENV === 'development' ? true : p.date <= now;
+  },
+};
+
+module.exports = {
+    formatDate: (date, format) => {
+        return DateTime.fromJSDate(date, { zone: 'utc' }).toFormat(format);
+    },
+
+    absoluteUrl: (url, base) => {
+        return new URL(url, base).toString();
+    },
+
+    filterLivePosts: (collection) => {
+        return collection.filter(helpers.isLivePost);
+    },
+
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    htmlDateString: (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+    },
+};
