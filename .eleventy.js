5f6db11513324c0ff456321f49234b31e5e7694f
Vadym Mariiechko <vadym.mariiechko@addepto.com>
2025-08-02 16:17:24 +0200

    Add Eleventy configuration for static asset management; add favicon files and web manifest

--- .eleventy.js
+++ .eleventy.js
@@ -7,8 +7,10 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addShortcode(shortcodeName, shortcodes[shortcodeName]);
   });
 
-  // Passthrough copy for co-located post assets and static files
-  eleventyConfig.addPassthroughCopy('src/static');
+  // Passthrough copy for static assets
+  eleventyConfig.addPassthroughCopy({ 'src/static/favicons': 'favicons' });
+  eleventyConfig.addPassthroughCopy('src/robots.txt');
+  eleventyConfig.addPassthroughCopy('src/manifest.webmanifest');
   eleventyConfig.addPassthroughCopy('src/content/posts/**/*.jpg');
   eleventyConfig.addPassthroughCopy('src/content/posts/**/*.png');
 
@@ -25,18 +27,14 @@ module.exports = function (eleventyConfig) {
   });
 
   return {
-    // Set custom directories for input, output, includes, and data
     dir: {
-      input: 'src',
-      output: '_site',
-      includes: '_includes',
-      data: '_data',
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data",
     },
-    // Define the template formats to be processed
-    templateFormats: ['md', 'njk', 'html'],
-    // Specify Nunjucks as the engine for Markdown files
-    markdownTemplateEngine: 'njk',
-    // Specify Nunjucks as the engine for HTML files
-    htmlTemplateEngine: 'njk',
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
   };
 };
